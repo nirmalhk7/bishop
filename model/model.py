b4d8abaa27c4d44dcb21983d77b50dc45151c093
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-03-26 23:02:24 -0600

    model fix
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- model/model.py
+++ model/model.py
@@ -8,8 +8,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
-
-
 # Generate synthetic data
 fake = Faker()
 num_samples = 15000  # 100 days of data, collected every 10 minutes
