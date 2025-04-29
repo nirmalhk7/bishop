fe08dba3cc9b84feebdfb2faae1d937811ae8016
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-04-28 21:31:25 -0600

    settings fix
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 
-
 @Injectable()
 export class AppService {
   getHello(): string {
@@ -10,8 +9,12 @@ export class AppService {
   }
 
   getSettings(): SettingsInterface {
-    const filePath = path.resolve(__dirname, '../settings.json');
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    return JSON.parse(fileContent);
+    if (process.env.NODE_ENV !== 'production') {
+      const filePath = path.resolve(__dirname, '../settings.json');
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      return JSON.parse(fileContent);
+    } else {
+      return JSON.parse(process.env.BISHOP_SETTINGS || "{}")
+    }
   }
 }
