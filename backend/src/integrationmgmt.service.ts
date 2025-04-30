21763800242493059a0581bb133233fea9a54cc6
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-04-30 00:39:11 -0600

    integration fix
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- backend/src/integrationmgmt.service.ts
+++ backend/src/integrationmgmt.service.ts
@@ -32,10 +32,7 @@ export class IntegrationMgmtService {
   }
 
   private resolveModulePath(endpointPath: string): string {
-    const basePath =
-      process.env.NODE_ENV === 'production'
-        ? path.join(__dirname, '..', '..', 'src')
-        : __dirname;
+    const basePath = process.env.NODE_ENV === 'production' ? 'dist' : __dirname;
     return path.resolve(basePath, endpointPath);
   }
 
@@ -90,4 +87,3 @@ export class IntegrationMgmtService {
     }
   }
 }
-
