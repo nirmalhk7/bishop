2f9ddc43bc6a048b7605a54f5f938cebff03af04
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-04-29 20:07:58 -0600

    fix: removed hardcoded test parts
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- backend/src/integrations/googlecalendar.service.ts
+++ backend/src/integrations/googlecalendar.service.ts
@@ -1,48 +1,12 @@
-// import { HttpService } from '@nestjs/axios';
-// import { Injectable, Logger } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-// import { firstValueFrom } from 'rxjs';
-// import { IntegrationInterface } from 'src/interfaces/integrations.interface';
-// import axios from 'axios';
-// import { Coordinates } from 'src/interfaces/global.interface';
-
-// @Injectable()
-// export default class GoogleCalendarService implements IntegrationInterface {
-//   private accessToken;
-//   constructor(
-//   ) {
-//     this.accessToken="AIzaSyAAq8L3ou_L5577MEN6XDiIPw36VrEzLUM"
-//   }
-//   private log= new Logger(GoogleCalendarService.name);
-
-  
-//   async get(start: Coordinates, end: Coordinates): Promise<any> {
-//     const url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
-
-//     return await axios.get(url, {
-//       headers: {
-//         Authorization: `Bearer ${this.accessToken}`,
-//       },
-//       params: {
-//         maxResults: 5,
-//         orderBy: 'startTime',
-//         singleEvents: true,
-//         timeMin: new Date().toISOString(),
-//       },
-//     })
-//     .then(resp=> resp.data)
-//     .catch(err=> this.log.error(err));
-//   }
-// }
-
-
-
-
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+import { IntegrationInterface } from 'src/interfaces/integrations.interface';
+import axios from 'axios';
+import { Coordinates } from 'src/interfaces/global.interface';
 import { google } from 'googleapis';
 import * as path from 'path';
-import { IntegrationInterface } from 'src/interfaces/integrations.interface';
-import { Coordinates } from 'src/interfaces/global.interface';
 
 @Injectable()
 export default class GoogleCalendarService implements IntegrationInterface {
@@ -50,7 +14,7 @@ export default class GoogleCalendarService implements IntegrationInterface {
   private calendar;
 
   constructor() {
-    const keyFilePath = path.join(__dirname, '../../calendar-service-account-key.json');
+    const keyFilePath = path.join(__dirname, '../../bdarch-bishop-calendar-svc.gcp.json');
 
     const auth = new google.auth.GoogleAuth({
       keyFile: keyFilePath,
@@ -73,7 +37,7 @@ export default class GoogleCalendarService implements IntegrationInterface {
 
       const events = response.data.items || [];
       this.log.debug(`Fetched ${events.length} events from calendar.`);
-
+      console.log(events)
       return events;
     } catch (error: any) {
       this.log.error('Error fetching calendar events', error);
